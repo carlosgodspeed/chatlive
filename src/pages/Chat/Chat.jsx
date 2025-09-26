341373c345becf4d60d2cbd5f01d56c881d8ddb5
carlosgodspeed <carlosminibics@gmail.com>
2025-09-26 16:26:41 -0300

    FEAT = criação de pastas e da Tela de Login

---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chat.css'
+
+const Chat = () => {
+  return (
+    <div>
+        estou aqui no chat
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileUpdate.css'
+
+const ProfileUpdate = () => {
+  return (
+    <div>
+        estou no perfil 
+    </div>
+  )
+}
+
+export default ProfileUpdate
