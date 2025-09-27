2f3501706849388d52dd88f12c24fe9d5ae196fc
carlosgodspeed <carlosminibics@gmail.com>
2025-09-26 22:26:55 -0300

    FEAT = Criação de Login e Cadastro

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 
 const Login = () => {
+
+  const [currState,setCurrState] = useState("Cadastre-se");
+
   return (
     <div className='login'>
         <img src={assets.logo_big} alt='' className='logo' />
+        <form className='login-form'>
+          <h2>{currState}</h2>
+          {currState === "Cadastre-se"?<input type='text' placeholder='Nome Completo' className='form-input' required/>:null}
+          <input type='email' placeholder='Email' className='form-input' required/>
+          <input type='password' placeholder='Senha' className='form-input' required/>
+          <button type='submit'>{currState === "Cadastre-se"?"Cadastrar":"Entrar"}</button>
+          <div className='login-term'>
+            <input type='checkbox'/>
+            <p>Concorda com os Termos de Uso e a Política de Privacidade?</p>
+          </div>
+          <div className='login-forgot'>
+            <p className='login-toggle'>Já possui uma conta? <span onClick={()=>setCurrState("Login")}>Clique aqui</span></p>
+          </div>
+        </form>
     </div>
   )
 }
