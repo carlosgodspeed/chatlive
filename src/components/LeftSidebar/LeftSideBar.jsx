73de920cafaab7817dc83c454f382389ed6be9a3
carlosgodspeed <carlosminibics@gmail.com>
2025-10-21 18:24:32 -0300

    Criando o Chat de Conversa

--- src/components/LeftSidebar/LeftSideBar.jsx
+++ src/components/LeftSidebar/LeftSideBar.jsx
@@ -18,13 +18,15 @@ const LeftSideBar = () => {
         </div>
       </div>
       <div className='ls-list'>
-        <div className='friends'>
-          <img src={assets.profile_img} alt='' />
-          <div>
-            <p>Richard Sanford</p>
-            <span>Hello, How are you</span>
+        {Array(12).fill("").map((item,index)=>(
+          <div key={index} className='friends'>
+            <img src={assets.profile_img} alt='' />
+            <div>
+              <p>Richard Sanford</p>
+              <span>Hello, how are you</span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   )
