9276674eb94b7cdca23b702793a54cfb65b2e215
carlosgodspeed <carlosminibics@gmail.com>
2025-10-22 18:24:08 -0300

    Estilizando os Componentes

--- src/components/LeftSidebar/LeftSideBar.jsx
+++ src/components/LeftSidebar/LeftSideBar.jsx
@@ -9,7 +9,12 @@ const LeftSideBar = () => {
         <div className='ls-nav'>
           <img src={assets.logo} className='logo' alt='' />
           <div className='menu'>
-            <img src={assets.menu_icon} alt='' />
+            <img src={assets.menu_icon} alt='' /> 
+            <div className='sub-menu'>
+              <p>Edit Profile</p>
+              <hr/>
+              <p>Logout</p>
+            </div>
           </div>
         </div>
         <div className='ls-search'>
